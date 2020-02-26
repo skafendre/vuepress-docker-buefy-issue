8423f78db1c967891704aaa75c09f9a6df3d285f
Eliott <eliot.lambert@gmail.com>
2020-02-26 17:39:20 +0100

    added files

---
+++ test/.vuepress/theme/Layout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="layout">
+    <Hello></Hello>
+  </div>
+</template>
+
+<script>
+  import Hello from '../components/Hello'
+  export default {
+      components: { Hello }
+  }
+</script>
